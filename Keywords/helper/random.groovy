3764efcd1a579dd8c0a9570be29975f3e63c3219
Joanna <joannasqae@gmail.com>
2023-07-30 23:39:51 +0700

    Add keywords/helper/random.groovy for random email/password generator

---
+++ Keywords/helper/random.groovy
@@ -0,0 +1,36 @@
+package helper
+
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+
+import com.kms.katalon.core.annotation.Keyword
+import com.kms.katalon.core.checkpoint.Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.testcase.TestCase
+import com.kms.katalon.core.testdata.TestData
+import com.kms.katalon.core.testobject.TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import org.apache.commons.lang.RandomStringUtils as RandomStringUtils
+import internal.GlobalVariable
+
+public class random {
+	@Keyword(keywordObject = "random generator")
+	public String email() {
+		String prefix = "user"
+		String domain = "sample.com"
+		String randomString = RandomStringUtils.randomAlphanumeric(5)
+		return "${prefix}${randomString}@${domain}"
+	};
+
+	@Keyword(keywordObject = "random generator")
+	public String password(int length) {
+		return RandomStringUtils.randomAlphanumeric(length)
+	}
+}
